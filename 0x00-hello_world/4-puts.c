7f34759d1cd8a91c5303d2745b1a31850e0aba5f
root <momostafa110022@gmail.com>
2023-02-20 15:37:54 -0800

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+/* printf func */
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
+}
