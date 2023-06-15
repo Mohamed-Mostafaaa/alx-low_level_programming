aa4f52ee15ffb1785b58515d9309c038ecebab8a
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-16 00:03:27 +0300

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Here is 1
+ */
+
+int main(void)
+{
+    putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+    return (1);
+}
