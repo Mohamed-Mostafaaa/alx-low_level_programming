48c14f3c558d6aa014e610e56e0bf5059f49711c
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-16 00:42:35 +0300

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 1
- */
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
+*/
 
 int main(void)
 {
-    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
