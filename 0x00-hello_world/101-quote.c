79766e184b56b54713750eda0926f4ab84d19213
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-16 00:48:33 +0300

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
@@ -11,6 +12,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
