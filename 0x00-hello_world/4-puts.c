89d0d4bc07dce993a4e86153040fa2af7ee962a7
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-15 21:58:43 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int main{
-	puts("\"Programming is like building a multilingual puzzle")
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
