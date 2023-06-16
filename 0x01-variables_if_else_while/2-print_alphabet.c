067c2c1b3ae97384320227744c383514bf758628
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-16 15:49:30 +0300

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n; greater than, is zero , not less than 6.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/* my code */
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+
+	return (0);
+}
