53e1d270238f3e357ff9981a08f203434257f4ec
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-16 16:24:31 +0300

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n; greater than, is zero , not less than 6.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* my code */
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
