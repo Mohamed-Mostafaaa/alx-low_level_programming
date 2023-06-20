bde98268e66ceb52a1112a4ee5fbc7662ea39370
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-20 18:29:28 +0300

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sum of even-valued
+ *		Fibonacci sequence not exceed
+ *		4million
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	long fib1 = 0, fib2 = 1, sum, total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%d\n", total_sum);
+
+	return (0);
+}
