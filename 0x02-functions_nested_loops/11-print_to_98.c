7030ac0459508977e1ead59c4264a6f02f098b10
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-06-20 18:09:09 +0300

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,10 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-		for (count = n; count >= 98; count--)
+		for (count = n; count > 98; count--)
 			printf("%d, ", count);
 	else
-		for (count = n; count <= 98; count++)
+		for (count = n; count < 98; count++)
 			printf("%d, ", count);
-	printf("\n");
+	printf("98\n");
 }
