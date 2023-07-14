c30f06b600f9e65da06dd66bc726050278308bfa
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-07-14 03:43:25 +0300

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -10,4 +11,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+#endif/*MAIN_H*/
